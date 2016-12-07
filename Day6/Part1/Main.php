bdb635bcdf7ac1ec90e8f4c406ba111665bb0a07
Łukasz Lalik <lukasz.lalik@gmail.com>
2016-12-07 07:33:58 +0100

    Day 6 Part 1 implementation

---
+++ Day6/Part1/Main.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace AOC\Day6\Part1;
+
+class Main
+{
+    public function run(string $input): string
+    {
+        $signals = explode(PHP_EOL, $input);
+        $signals = array_filter($signals, function (string $signal): bool {
+            return strlen($signal) > 0;
+        });
+
+        $charactedCouts = [];
+
+        foreach ($signals as $signal) {
+            for ($i = 0; $i < strlen($signal); $i++) {
+                $char = $signal[$i];
+                if (!isset($charactedCouts[$i][$char])) {
+                    $charactedCouts[$i][$char] = 0;
+                }
+                $charactedCouts[$i][$char]++;
+            }
+        }
+
+        $charactedCouts = array_map(function (array $counts): array {
+            asort($counts);
+            return $counts;
+        }, $charactedCouts);
+
+        return array_reduce($charactedCouts, function (string $password, array $counts): string {
+            return $password . array_keys($counts)[0];
+        }, '');
+    }
+}
---
+++ Tests/Day6/Part1/MainTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace AOC\Tests\Day6\Part1;
+
+use AOC\Day6\Part1\Main;
+
+class MainTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_run()
+    {
+        $input = "eedadn\ndrvtee\neandsr\nraavrd\natevrs\ntsrnev\nsdttsa\nrasrtv\nnssdts\nntnada\nsvetve\ntesnvt\nvntsnd\nvrdear\ndvrsen\nenarar\n";
+        $expectedOutput = 'easter';
+
+        $main = new Main();
+        $output = $main->run($input);
+
+        $this->assertEquals($expectedOutput, $output);
+    }
+}
