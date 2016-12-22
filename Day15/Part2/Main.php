1da1ed169e11fee0b80886466602d991d4736d47
Łukasz Lalik <lukasz.lalik@gmail.com>
2016-12-22 15:28:30 +0100

    Day 15 Part 2 implementation

---
+++ Day15/Part2/Main.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace AOC\Day15\Part2;
+
+use AOC\Day15\Part1\DiscFunction;
+use AOC\Day15\Part1\Parser;
+
+class Main
+{
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    public function __construct()
+    {
+        $this->parser = new Parser();
+    }
+
+    public function run(string $input): int
+    {
+        $inputSets = explode("\n", $input);
+        $inputSets = array_filter($inputSets, function (string $inputSet): bool {
+            return strlen($inputSet) > 0;
+        });
+
+        $functionsParameters = array_map([$this->parser, 'parse'], $inputSets);
+        /** @var DiscFunction[] $functions */
+        $functions = array_map([$this, 'makeFunction'], $functionsParameters);
+        array_push($functions, new DiscFunction(11, 0));
+
+        $result = 0;
+
+        for ($i = 0; $result == 0; $i++) {
+            for ($j = 0; $j < count($functions); $j++) {
+                $t = $j + $i;
+                $result = $i;
+                if ($functions[$j]->at($t) !== 0) {
+                    $result = 0;
+                    break;
+                }
+            }
+        }
+
+        return $result - 1;
+    }
+
+    private function makeFunction(array $parameters): DiscFunction
+    {
+        return new DiscFunction($parameters[0], $parameters[1]);
+    }
+}
